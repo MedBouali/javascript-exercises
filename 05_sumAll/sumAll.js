bbb34f3e20c2c29198a836c729b46adac816f5af
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-12 06:40:00 +0100

    Update sumAll.js for 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstInteger, lastInteger) {
+    let sum = 0, temp = 0;
 
+    if(Number.isInteger(firstInteger) && Number.isInteger(lastInteger) && firstInteger > 0 && lastInteger > 0) {
+        if(firstInteger > lastInteger) {
+            temp = firstInteger;
+            firstInteger = lastInteger;
+            lastInteger = temp;
+        }
+
+        for(let i = firstInteger ; i <= lastInteger ; i++) {
+            sum += i;
+        }
+    } else {
+        return "ERROR";
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
