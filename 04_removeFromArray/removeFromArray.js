e8e1cf1f967c9d5332edd54ee4ffb27f750bc6cb
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-12 06:07:08 +0100

    Update removeFromArray.js for 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToDelete) {
+    newArray = [];
+
+    for(let i = 0 ; i < array.length ; i++) {
+        if(!valuesToDelete.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    
+    return newArray;
 };
 
 // Do not edit below this line
