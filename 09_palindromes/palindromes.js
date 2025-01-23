9b7fc086f57230c35a13902e4e4e890d5ac2932d
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-23 15:58:27 +0100

    Update palindromes.js for 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    let isPalindrome = true;
+    const possibleCharacters = "azertyuiopqsdfghjklmwxcvbn0123456789";
+    let string = inputString
+        .toLowerCase()
+        .split('')
+        .filter(char => possibleCharacters.includes(char));
 
+    for(let i = 1 ; i <= string.length / 2 ; i++) {
+        if(string[i - 1] !== string[string.length - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    
+    return isPalindrome;
 };
 
 // Do not edit below this line
