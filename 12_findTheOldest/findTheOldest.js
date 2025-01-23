a95131402b8a539cfa65820b2b09ccf37557dc59
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-23 21:02:38 +0100

    Update findTheOldest.js for 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const yearNow = new Date().getFullYear();
 
+    const oldestPerson = people
+        .sort((firstPerson, secondPerson) => {
+            let firstPersonYearOfDeath = firstPerson.yearOfDeath;
+            let secondPersonYearOfDeath = secondPerson.yearOfDeath;
+
+            if(typeof firstPersonYearOfDeath === 'undefined') {
+                firstPersonYearOfDeath = yearNow;
+            }
+
+            if(typeof secondPersonYearOfDeath === 'undefined') {
+                secondPersonYearOfDeath = yearNow;
+            }
+            
+            const firstPersonAge = firstPersonYearOfDeath - firstPerson.yearOfBirth;
+            const secondPersonAge = secondPersonYearOfDeath - secondPerson.yearOfBirth;
+            
+            return secondPersonAge - firstPersonAge;
+        })[0];
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
