ff1264a6021509729ff3b2febc47a2eb9652e593
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-23 19:35:14 +0100

    Update getTheTitles.js for 11_getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    titlesArray = array.map(prop => prop["title"]);
+    return titlesArray;
 };
 
 // Do not edit below this line
