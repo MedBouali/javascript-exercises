959a52e36693719dea1ad3f4f45f0aaba68b366b
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-23 17:54:17 +0100

    Update fibonacci.js for 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    index = parseInt(input);
+    
+    if(index >= 0){
+        let array = [0, 1];
+        for(let i = 2 ; i <= index ; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
 
+        return array[index];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
